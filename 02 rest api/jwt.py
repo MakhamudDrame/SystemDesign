a06af5b9b4b6a0c803c75d4ad046be678c4f6ef4
MakhamudDrame <naive1899@gmail.com>
2024-10-27 16:08:35 +0300

    Update jwt.py

--- 02 rest api/jwt.py
+++ 02 rest api/jwt.py
@@ -33,16 +33,8 @@ service_db = []
 
 # Псевдо-база данных пользователей
 client_db = {
-    "admin":
-    {   "Id": 1,
-        "username": "admin",
-        "email": "admin@gmail.com",
-        "hashed_password":  "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",  # hashed "secret"
-        "age":30
-    }
+    "admin": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"  # hashed "secret"
 }
-
-
 
 
 # Настройка паролей
@@ -51,32 +43,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Настройка OAuth2
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# Создание и проверка JWT токенов
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-# Получение пользователя из clien_db
-def get_user_from_db(client_db, username: str):
-    if username in client_db:
-        user_dict = client_db[username]
-        return User(**user_dict)
-        
-# Аутентификация
-def authenticate_user(client_db, username: str, password: str):
-    user= get_user_from_db(client_db, username)
-    if not user:
-        return False
-    if not pwd_context.verify(password, user.hashed_password):
-        return False
-    return user
-    
 # Зависимости для получения текущего пользователя
 async def get_current_client(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -89,28 +55,41 @@ async def get_current_client(token: str = Depends(oauth2_scheme)):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
+        else:
+            return username
     except JWTError:
         raise credentials_exception
-    user = get_user_from_db(client_db, username=username)
-    if user is None:
-        raise credentials_exception
-    return user
+
+# Создание и проверка JWT токенов
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 # Маршрут для получения токена
 @app.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(client_db, form_data.username, form_data.password)
-    if not user:
+    password_check = False
+    if form_data.username in client_db:
+        password = client_db[form_data.username]
+        if pwd_context.verify(form_data.password, password):
+            password_check = True
+
+    if password_check:
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(data={"sub": form_data.username}, expires_delta=access_token_expires)
+        return {"access_token": access_token, "token_type": "bearer"}
+    else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-        )
-    return {"access_token": access_token, "token_type": "bearer"}
 
 # GET /users - Получить всех пользователей (требует аутентификации)
 @app.get("/users", response_model=List[User])
