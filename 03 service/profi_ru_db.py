b58ae3952f5ba56bbaf19fc18a01d81583cb00a9
MakhamudDrame <naive1899@gmail.com>
2024-11-15 12:19:23 +0300

    Update profi_ru_db.py

--- 03 service/profi_ru_db.py
+++ 03 service/profi_ru_db.py
@@ -54,11 +54,13 @@ class Order(BaseModel):
 class UserDB(Base):
     __tablename__ = 'users'
     
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    age = Column(Integer)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    email = Column(String, unique=True, index=True)
+    
     
 # Определение модели услуги
 class ServiceDB(Base):
